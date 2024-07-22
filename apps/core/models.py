831bcd1a8478885fb4c3f4bb422bbd1ba3cf5cf5
Mobin <mobin.clash.ma@gmail.com>
2024-07-23 02:48:31 +0330

    fix: Add uid field for best security of finding objects

--- apps/core/models.py
+++ apps/core/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-    id = models.UUIDField(_("id"), default=uuid.uuid4, editable=False)
+    uid = models.UUIDField(_("uid"), default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(_("created_at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("updated_at"), auto_now=True)
     is_deleted = models.BooleanField(_("is_deleted"), default=False, editable=False)
