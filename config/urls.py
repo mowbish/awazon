ae5f1865ece01b20497ac5a25043b95d56ad07b3
Mobin <mobin.clash.ma@gmail.com>
2024-07-23 02:54:34 +0330

    feat: Specifying base routes of project

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        f"api/v1/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema-v1",
+    ),
+    path(
+        f"",
+        SpectacularSwaggerView.as_view(url_name="schema-v1"),
+        name="swagger-ui-v1",
+    ),
+    path(f"api/v1/", include("config.routers")),
+
 ]
