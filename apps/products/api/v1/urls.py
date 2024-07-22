0a29c7573f7221e77d4e743d5857b934dc4d1fa7
Mobin <mobin.clash.ma@gmail.com>
2024-07-23 02:49:49 +0330

    feat: Add products route

---
+++ apps/products/api/v1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from apps.products.api.v1.views import ProductViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', ProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
