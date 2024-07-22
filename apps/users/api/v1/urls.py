b2f0989ca2f7fb8e6b7c6e574030e59bd6e18bbb
Mobin <mobin.clash.ma@gmail.com>
2024-07-23 02:53:06 +0330

    feat: Add routes of UserViewSet and getting access_token & refresh_token

---
+++ apps/users/api/v1/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from apps.users.api.v1.views import UserViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+router = DefaultRouter()
+router.register(r'', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
