5514b67e88e1419439c27a5b582fc28b4f973bef
Mobin <mobin.clash.ma@gmail.com>
2024-07-23 02:53:46 +0330

    feat: Add User model with inheritacing from DjangoAbstructUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,23 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from apps.core.models import BaseModel
+
+
+class User(AbstractUser):
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    is_suspended = models.BooleanField(default=False)
+    gender = models.CharField(
+        choices=[("MALE", _("MALE")), ("FEMALE", _("FEMALE"))],
+        null=True,
+        blank=True,
+        max_length=220,
+    )
+    auth_level = models.SmallIntegerField(default=0)
+    phone_number = models.CharField(max_length=11, unique=True)
+    birthday = models.DateField(null=True, blank=True)
